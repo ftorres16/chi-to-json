5cdde797865d072d00a6a26fa6fb0b5e5faf3bb1
ftorres16 <fabian.torres.alvarez+github@gmail.com>
2019-05-10 22:38:12 -0300

    added jsonschema dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     py_modules=["chi_to_json"],
     include_package_data=True,
-    install_requires=["click"],
+    install_requires=["click", "jsonschema"],
     entry_points="""
         [console_scripts]
         chi_to_json=chi_to_json.cli:cli
